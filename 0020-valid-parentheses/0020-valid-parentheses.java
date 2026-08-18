55ceb825d83bfab155478a2f9fab07550677aa91
Mrudweeka <mrudweeka1905@gmail.com>
2026-08-18 11:36:24 +0530

    Time: 3 ms (86.06%), Space: 42.9 MB (96.9%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack=new Stack<>();
+         for(int i=0;i<s.length();i++){
+            char x=s.charAt(i);
+            if(x=='(' ||x=='[' || x=='{'){
+                stack.push(x);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top=stack.pop();
+                if(x==')' && top!='(' || x=='}' && top!='{' || x==']' && top!='['){
+                    return false;
+                }
+            }
+         }
+         return stack.isEmpty();
+    }
+}
